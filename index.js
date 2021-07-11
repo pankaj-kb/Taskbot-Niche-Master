489602738494a3b9ed10c97590d1071875f7bef2
Pankaj Bhadane <pankaj-kb>
2021-07-11 12:45:13 +0530

    modification at gitignore

--- index.js
+++ index.js
@@ -33,4 +33,16 @@ bot.on('message', (msg) => {
             });
             
             });
-            
+            const express = require('express')
+const bodyParser = require('body-parser');
+ 
+const app = express();
+ 
+app.use(bodyParser.json());
+ 
+app.listen(process.env.PORT);
+ 
+app.post('/' + bot.token, (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
