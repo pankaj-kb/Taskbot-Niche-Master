9eb51fd1bfb39bc282a2b2e6aba7dd0458601fe0
Pankaj Bhadane <pankaj-kb>
2021-07-15 01:09:10 +0530

    adding quotes

--- quotes.js
+++
@@ -1,6 +0,0 @@
-let inspi = await fetch(/txt/)
-inspi.get('txt/messages.txt', function(txt) {
-    var lines = txt.responseText.split("\n");
-    var randLineNum = Math.floor(Math.random() * lines.length);
-    return lines[randLineNum]; // random line from the text file
-    })
