5cb3f1e5f0fe8137f781a8d3594b1e017c214c05
Pankaj Bhadane <pankaj-kb>
2021-07-16 16:15:31 +0530

    changes

---
+++ insight.js
@@ -0,0 +1,16 @@
+// var quotes = require('./quotes');
+// var child_process = require('child_process');
+// child_process.exec('node quotes.js', (error, stdout, stderr) => {
+//     console.log(`${stdout}`);
+//     console.log(`${stderr}`);
+//     if (error !== null) {
+//         console.log(`exec error: ${error}`);
+//     }
+// });
+const {PythonShell} = require('python-shell');
+
+var pyshell = new PythonShell('insight.py');
+
+  pyshell.on('message', function(insight) {
+  console.log(insight);
+})
