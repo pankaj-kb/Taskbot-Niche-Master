204928bba714803b4d599a4a2f61b56f5c7774b4
Pankaj Bhadane <pankaj-kb>
2021-07-20 18:02:27 +0530

    fixed minor issues

--- bot.js
+++ bot.js
@@ -121,49 +121,8 @@ bot.on('message', async (msg) => {
             });
         }
     }
-   var greet = "hi";
-    if (msg.text.toString().toLowerCase().includes(greet) || msg.text.toString().toLowerCase().includes("hello") || msg.text.toString().toLowerCase().includes("hey") ) {
-    bot.sendMessage(msg.chat.id,"hey !! "+ msg.from.first_name);
-    }
-    var bye = "bye";
-    if (msg.text.toString().toLowerCase().includes(bye) || msg.text.toString().toLowerCase().includes("see yaa")) {
-        bot.sendMessage(msg.chat.id, " bye !! " + msg.from.first_name + " Do Something Great");
-    }
-    var formality = "how are you";
-    if (msg.text.toString().toLowerCase().includes(formality) || msg.text.toString().toLowerCase().includes("how are you ?")) {
-        bot.sendMessage(msg.chat.id, "I am having great time , Just Like a 🤖");
-    }
-
-    var appreciate = "thanks";
-    if (msg.text.toString().toLowerCase().includes(appreciate) || msg.text.toString().toLowerCase().includes("well done") || msg.text.toString().toLowerCase().includes("♥")) {
-        bot.sendMessage(msg.chat.id, "Its a Pleasure ♥");
-    }
-    var appreciate2 = "good job";
-    if (msg.text.toString().toLowerCase().includes(appreciate2)){
-        bot.sendMessage(msg.chat.id, "Its a Pleasure ♥");
-    }
-    var ok = "ok";
-    if (msg.text.toString().toLowerCase().includes(ok) || msg.text.toString().toLowerCase().includes("👍")){
-        bot.sendMessage(msg.chat.id, "👍");
-    }
-    var hehe = "hehe";
-    if (msg.text.toString().toLowerCase().includes(hehe)){
-        bot.sendMessage(msg.chat.id, "😉");
-    }
-    var viewTasks = "view tasks";
-    if (msg.text.toString().toLowerCase().includes(viewTasks)){
-        bot.sendMessage(msg.chat.id, "Feature is Upcoming");
-    }
-    var setupTasks = "set-up tasks";
-    if (msg.text.toString().toLowerCase().includes(setupTasks)){
-        bot.sendMessage(msg.chat.id, "Feature is Upcoming");
-    }
-  var accomplitioments = "recent accomplitioments";
-    if (msg.text.toString().toLowerCase().includes(accomplitioments)){
-        bot.sendMessage(msg.chat.id, "Feature is Upcoming");
-    }
-})
-  bot.onText(/\/start/, (msg) => {
+    }) 
+ bot.onText(/\/start/, (msg) => {
     bot.sendMessage(msg.chat.id, "Gear Up " + msg.from.first_name + " Lets Get Productive", {
         'reply_markup': {
             'keyboard': [['📝 View Tasks', '✍ Set-Up Tasks'], ['🏆 Recent Accomplitioments'], ['🧠💭 Lessons']],
